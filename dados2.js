348f5cb2535b75efc6d883eb7f1dbdf0ec16ad44
lucasody96 <lucasody@gmail.com>
2021-11-27 09:49:51 -0300

    fundamentos

---
+++ dados2.js
@@ -0,0 +1,13 @@
+let idade = 31;
+
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 457.832;
+console.log(typeof salario);
+
+let estaChovendo = true;
+console.log(typeof estaChovendo);
+
+console.log(typeof "teste");
+console.log(typeof 'teste');
---
+++ desafioArea.js
@@ -0,0 +1,9 @@
+//PI * raio * raio
+const pi = 3.141592
+let raio = 10;
+
+let area = pi * raio * raio;
+
+console.log ("O valor da área é " + area + "M².");
+
+console.log(Math.PI);
